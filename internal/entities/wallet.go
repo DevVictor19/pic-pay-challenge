38e5ed5169850de4ce7b1dc4abc33f72c06a521a
DevVictor19 <antoniovictor12@live.com>
2025-05-19 18:10:10 -0300

    fix: field typo

--- internal/entities/wallet.go
+++ internal/entities/wallet.go
@@ -5,7 +5,7 @@ import "time"
 type Wallet struct {
 	ID        int       `json:"id"`
 	UserID    int       `json:"user_id"`
-	Ative     bool      `json:"active"`
+	Active    bool      `json:"active"`
 	Balance   int64     `json:"balance"`
 	UpdatedAt time.Time `json:"updated_at"`
 	CreatedAt time.Time `json:"created_at"`
