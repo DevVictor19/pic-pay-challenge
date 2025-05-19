f7f7957d07ae089e10b9bf66bf4e03c2c9f5ba82
DevVictor19 <antoniovictor12@live.com>
2025-05-19 18:06:15 -0300

    feat: add transaction entity

---
+++ internal/entities/transaction.go
@@ -0,0 +1,21 @@
+package entities
+
+import "time"
+
+type TransactionType string
+
+const (
+	TRAN_PAYMENT_RECEIVED = "payment_received"
+	TRAN_PAYMENT_SENT     = "payment_sent"
+)
+
+type Transaction struct {
+	ID          int             `json:"id"`
+	PayerID     int             `json:"payer_id"`
+	PayeeID     int             `json:"payee_id"`
+	Type        TransactionType `json:"type"`
+	Amount      int64           `json:"amount"`
+	Description string          `json:"description"`
+	UpdatedAt   time.Time       `json:"updated_at"`
+	CreatedAt   time.Time       `json:"created_at"`
+}
