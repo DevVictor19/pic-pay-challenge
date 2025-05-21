1116db3c0559798c7efcf69c44a1dadaf35c23cb
DevVictor19 <antoniovictor12@live.com>
2025-05-21 18:28:27 -0300

    feat: add  errors codes

---
+++ internal/infra/http_errors/errors.go
@@ -0,0 +1,12 @@
+package http_errors
+
+import "errors"
+
+type HttpErrCode error
+
+var (
+	ErrNotFound   HttpErrCode = errors.New("NOT_FOUND")
+	ErrBadRequest HttpErrCode = errors.New("BAD_REQUEST")
+	ErrConflict   HttpErrCode = errors.New("CONFLICT")
+	ErrInternal   HttpErrCode = errors.New("INTERNAL")
+)
