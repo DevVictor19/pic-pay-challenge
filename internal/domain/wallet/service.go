000a869d2898f7557f20957df02af362e84178d8
DevVictor19 <antoniovictor12@live.com>
2025-05-21 18:35:54 -0300

    feat: implement Create method

--- internal/domain/wallet/service.go
+++ internal/domain/wallet/service.go
@@ -1,13 +1,37 @@
 package wallet
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/DevVictor19/pic-pay-challenge/internal/infra/http_errors"
+)
+
 type WalletService interface {
 	Create(userID int, balance int64) error
 }
 
 type walletSvc struct {
+	wallRepo *WalletRepository
 }
 
 func (s *walletSvc) Create(userID int, balance int64) error {
+	wallRepo := *s.wallRepo
+
+	now := time.Now()
+	wall := Wallet{
+		UserID:    userID,
+		Active:    true,
+		Balance:   balance,
+		UpdatedAt: now,
+		CreatedAt: now,
+	}
+
+	err := wallRepo.Save(wall)
+	if err != nil {
+		return fmt.Errorf("error saving wallet: %w", http_errors.ErrInternal)
+	}
+
 	return nil
 }
 
@@ -15,7 +39,9 @@ var walletSvcRef *walletSvc
 
 func NewWalletService(wallRepo *WalletRepository) WalletService {
 	if walletSvcRef == nil {
-		walletSvcRef = &walletSvc{}
+		walletSvcRef = &walletSvc{
+			wallRepo,
+		}
 	}
 	return walletSvcRef
 }
