fc5c28d0709aac752e72724360f90add7b2e864f
DevVictor19 <antoniovictor12@live.com>
2025-05-22 08:41:11 -0300

    feat: add wallet validate

--- internal/domain/wallet/entity.go
+++ internal/domain/wallet/entity.go
@@ -1,6 +1,9 @@
 package wallet
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type Wallet struct {
 	ID        int       `json:"id"`
@@ -9,4 +12,28 @@ type Wallet struct {
 	Balance   int64     `json:"balance"`
 	UpdatedAt time.Time `json:"updated_at"`
 	CreatedAt time.Time `json:"created_at"`
+}
+
+func (w *Wallet) Validate() error {
+	if err := isValidUserID(w.UserID); err != nil {
+		return err
+	}
+	if err := isValidBalance(w.Balance); err != nil {
+		return err
+	}
+	return nil
+}
+
+func isValidUserID(userID int) error {
+	if userID <= 0 {
+		return errors.New("user id must be greater than 0")
+	}
+	return nil
+}
+
+func isValidBalance(balance int64) error {
+	if balance < 0 {
+		return errors.New("balance must be equal or greater than 0")
+	}
+	return nil
 }
