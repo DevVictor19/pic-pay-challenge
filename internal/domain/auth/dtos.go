41c2c0d74246919c5c420b763f3fd44550bfe621
DevVictor19 <antoniovictor12@live.com>
2025-05-24 15:31:07 -0300

    fix: bad syntax for json

--- internal/domain/auth/dtos.go
+++ internal/domain/auth/dtos.go
@@ -3,7 +3,7 @@ package auth
 type SignupDTO struct {
 	Fullname string  `json:"fullname" validate:"required,max=100"`
 	CPF      *string `json:"cpf,omitempty" validate:"omitempty,len=11"`
-	CNPJ     *string `json:"cnpj,omitempty" validate:"omitempty,len=14`
+	CNPJ     *string `json:"cnpj,omitempty" validate:"omitempty,len=14"`
 	Email    string  `json:"email" validate:"required,email,max=100"`
 	Password string  `json:"password" validate:"required,gte=6,max=100"`
 }
