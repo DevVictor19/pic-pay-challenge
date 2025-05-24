4c1a540125493c0aaece1adf91bd8da197565d2e
DevVictor19 <antoniovictor12@live.com>
2025-05-24 15:29:42 -0300

    feat: add user dtos

--- internal/domain/user/dtos.go
+++ internal/domain/user/dtos.go
@@ -1,15 +1,15 @@
 package user
 
 type CommonUserDTO struct {
-	Fullname string
-	CPF      string
-	Email    string
-	Password string
+	Fullname string  `json:"fullname" validate:"required,max=100"`
+	CPF      *string `json:"cpf,omitempty" validate:"omitempty,len=11"`
+	Email    string  `json:"email" validate:"required,email,max=100"`
+	Password string  `json:"password" validate:"required,gte=6,max=100"`
 }
 
 type ShopkeeperUserDTO struct {
-	Fullname string
-	CNPJ     string
-	Email    string
-	Password string
+	Fullname string  `json:"fullname" validate:"required,max=100"`
+	CNPJ     *string `json:"cnpj,omitempty" validate:"omitempty,len=14"`
+	Email    string  `json:"email" validate:"required,email,max=100"`
+	Password string  `json:"password" validate:"required,gte=6,max=100"`
 }
